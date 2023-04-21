dc35bea57867aa28bf68631976527ee8ddfd2a98
GithubADFDemo <126145270+GithubADFDemo@users.noreply.github.com>
2023-04-21 15:04:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ data "azuread_application_template" "dbx" {
 }
 
 resource "azuread_application" "scim" {
-  display_name = "scim-connector"
+  display_name = "scim-connector123"
   template_id  = data.azuread_application_template.dbx.template_id
   
   app_role {
