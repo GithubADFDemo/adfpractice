8a0d3c7008e4c13b30adc4adc0b4f21dcc776b84
GithubADFDemo <126145270+GithubADFDemo@users.noreply.github.com>
2023-04-25 17:33:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ data "azuread_application_template" "dbx" {
 resource "azuread_application" "scim" {
   display_name = "scim-dbx"
   template_id  = data.azuread_application_template.dbx.template_id
-  owners = [ data.azuread_client_config.current.object_id ]
+#   owners = [ data.azuread_client_config.current.object_id ]
   app_role {
     allowed_member_types = [ "User" ]
     description = "Users/Groups Permission"
@@ -38,4 +38,28 @@ resource "azuread_application" "scim" {
     enterprise = true
     gallery    = true
   }
+}
+
+resource "azuread_service_principal" "test" {
+  application_id = azuread_application.scim.application_id
+  use_existing   = true
+}
+
+resource "azuread_synchronization_secret" "example" {
+  service_principal_id = azuread_service_principal.test.id
+
+  credential {
+    key   = "BaseAddress"
+    value = "https://adb-2923826945884909.9.azuredatabricks.net/api/2.0/preview/scim"
+  }
+  credential {
+    key   = "SecretToken"
+    value = "dapi8f26127c0ee5b2f1289b2b41dd7764df-3"
+  }
+}
+
+resource "azuread_synchronization_job" "sync" {
+  service_principal_id = azuread_service_principal.test.id
+  template_id          = "dataBricks"
+  enabled              = true
 }
