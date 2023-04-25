bdcd09276b3278140ff3f17ed2e9afac0f8e8f92
GithubADFDemo <126145270+GithubADFDemo@users.noreply.github.com>
2023-04-25 18:17:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,14 @@ terraform {
       source  = "hashicorp/azuread"
       version = "2.37.1"
     }
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = "3.53.0"
-#     }
   }
 }
 
 provider "azuread" {
 }
-provider "azurerm" {
-  features {}
-}
+
 data "azuread_client_config" "current" {}
-# data "azurerm_client_config" "current" {}
+
 data "azuread_application_template" "dbx" {
   display_name = "Azure Databricks SCIM Provisioning Connector"
 }
@@ -25,7 +19,7 @@ data "azuread_application_template" "dbx" {
 resource "azuread_application" "scim" {
   display_name = "scim-dbx"
   template_id  = data.azuread_application_template.dbx.template_id
-#   owners = [ data.azuread_client_config.current.object_id ]
+
   app_role {
     allowed_member_types = [ "User" ]
     description = "Users/Groups Permission"
